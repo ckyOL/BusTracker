b5c48e815da86a2f78d24dfaaba165098a2f1f1f
Chen Ke Yu <gcky94@gmail.com>
2016-03-01 15:08:35 +0800

    Initial commit

---
+++ app/src/main/java/chen/studio/bustracker/MainActivity.java
@@ -0,0 +1,48 @@
+package chen.studio.bustracker;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        webView = (WebView)findViewById(R.id.web_view);
+        editText = (EditText)findViewById(R.id.edit_text);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view,String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String busNum = editText.getText().toString();
+                webView.loadUrl("http://www.openxdata.cn/XBusL/?busline_name="+busNum);
+            }
+        });
+    }
+
+}
